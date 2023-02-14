a3b95365645410671bbe6885f140925c3ec1e6c7
jinsi14 <jinsi.viradiya24@gmail.com>
2023-02-14 18:43:52 +0530

    Commit all exercise

--- Javascript/challenge-4/app.js
+++ Javascript/challenge-4/app.js
@@ -21,6 +21,5 @@ Hints:
 */
 
 const bill = 275;
-const tip = bill <= 300 && bill >= 50 ? bill *0.15:
-bill*0.2;
+const tip = bill <= 300 && bill >= 50 ? bill *0.15 : bill*0.2;
 console.log(`the bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
