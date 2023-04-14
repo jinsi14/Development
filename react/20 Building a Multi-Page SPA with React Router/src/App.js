d413664f5a917dc9d75facc32c0b758ec51ce9e9
jinsi14 <jinsi.viradiya24@gmail.com>
2023-04-14 19:00:47 +0530

    14-04-2023

--- react/20 Building a Multi-Page SPA with React Router/src/App.js
+++ react/20 Building a Multi-Page SPA with React Router/src/App.js
@@ -1,18 +1,23 @@
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
-    <div>
-      <Route path="/welcome">
-         <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
-    </div>
+    <BrowserRouter>
+      <MainHeader />
+      <main>
+      <Routes>
+        <Route path='/welcome' element={<Welcome />}> </Route>
+        <Route path='/products' element={<Products />}> </Route>
+        <Route path='/products/:productId' element={<ProductDetail />}></Route>
+      </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
 export default App;
+ 
--- react/20 Building a Multi-Page SPA with React Router/src/pages/Welcome.js
+++ react/20 Building a Multi-Page SPA with React Router/src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
 const Welcome = () => {
-    return <h1>The Welcome Page</h1>;
+    return (
+        <section>
+            <h1>The Welcome Page</h1>
+            <Routes>
+                <Route path="/welcome/new-user" element = {<p>welcome, new user!</p>} ></Route>
+            </Routes>
+        </section>
+    )
 };
 
 export default Welcome;
