e3a179c2e69796176ed9b67f83c9f0a041963677
jinsi14 <jinsi.viradiya24@gmail.com>
2023-04-15 12:42:46 +0530

    15-04-2023

---
+++ react/20 router practice project/src/pages/AllQuotes.js
@@ -0,0 +1,17 @@
+import QuoteList from '../components/quotes/QuoteList';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
+
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
+const AllQuotes = () => {
+  const {sendRequest, status, data: loadedQuote, error }= useHttp(getAllQuotes, true);
+
+  return <QuoteList quotes={DUMMY_QUOTES} />
+};
+
+export default AllQuotes;
