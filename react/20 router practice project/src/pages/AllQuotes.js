9a65530dd40ee03915b037e40e4d03a6a9b25e1d
jinsi14 <jinsi.viradiya24@gmail.com>
2023-04-17 18:19:04 +0530

    17-04-2023

--- react/20 router practice project/src/pages/AllQuotes.js
+++ react/20 router practice project/src/pages/AllQuotes.js
@@ -1,17 +1,37 @@
+import { useEffect } from 'react';
+
 import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 import useHttp from '../hooks/use-http';
 import { getAllQuotes } from '../lib/api';
-
-
-const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
-  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
-];
-
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+ 
 const AllQuotes = () => {
-  const {sendRequest, status, data: loadedQuote, error }= useHttp(getAllQuotes, true);
+  const {sendRequest, status, data: loadedQuotes, error }= useHttp(getAllQuotes, true);
 
-  return <QuoteList quotes={DUMMY_QUOTES} />
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if(status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if(error) {
+    return (
+      <p className='centered focused'>{error}</p>
+    )
+  }
+
+  if(status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound /> 
+  }
+
+  return <QuoteList quotes={loadedQuotes} />
 };
 
 export default AllQuotes;
